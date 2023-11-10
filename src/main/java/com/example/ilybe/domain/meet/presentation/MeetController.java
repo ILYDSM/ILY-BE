c228922beea0f257634dedc0235f330e9be3c005
csh0927 <sung060927@gmail.com>
2023-11-10 11:54:05 +0900

    MeetController 오타 수정

--- src/main/java/com/example/ilybe/domain/meet/presentation/MeetController.java
+++ src/main/java/com/example/ilybe/domain/meet/presentation/MeetController.java
@@ -3,6 +3,7 @@ package com.example.ilybe.domain.meet.presentation;
 import com.example.ilybe.domain.meet.presentation.dto.request.MeetCreateRequest;
 import com.example.ilybe.domain.meet.service.MeetCreateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class MeetController {
     private final MeetCreateService meetCreateService;
 
     @PostMapping
-    public Long MeetGenerate(@RequestBody @Valid MeetCreateRequest request) {
+    public Long MeetCreate(@RequestBody @Valid MeetCreateRequest request) {
         return meetCreateService.execute(request);
     }
+
+    @PatchMapping
+    public void Meet
 }

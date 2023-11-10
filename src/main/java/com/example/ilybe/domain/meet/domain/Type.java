b700d63d3b6fabaa5aa2e8f579c95ee7e9728c28
csh0927 <sung060927@gmail.com>
2023-11-10 10:14:17 +0900

    Type 수정
    관심사 입력함

--- src/main/java/com/example/ilybe/domain/meet/domain/Type.java
+++ src/main/java/com/example/ilybe/domain/meet/domain/Type.java
@@ -1,4 +1,6 @@
 package com.example.ilybe.domain.meet.domain;
 
 public enum Type {
+    Art, Sports, Health, Self_development, Career, Self_management, Money, Education, Environment, Relationship
+
 }

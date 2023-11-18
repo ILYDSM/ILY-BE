a52c0a7ea2e78f830812b28477c0851367f5a351
seongwon <seongwon1290@dsm.hs.kr>
2023-11-18 20:03:00 +0900

    만다라트 전체조회 API

---
+++ src/main/java/com/example/ilybe/domain/target/service/QueryTargetsService.java
@@ -0,0 +1,24 @@
+package com.example.ilybe.domain.target.service;
+
+import com.example.ilybe.domain.target.domain.repository.TargetRepository;
+import com.example.ilybe.domain.target.presentation.dto.response.QueryTargetsResponse;
+import com.example.ilybe.domain.user.domain.User;
+import com.example.ilybe.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QueryTargetsService {
+    private final TargetRepository targetRepository;
+    private final UserFacade userFacade;
+
+    public List<QueryTargetsResponse> execute() {
+        User user = userFacade.getCurrentUser();
+        return targetRepository.findAllByUserAndIsViewable(user, true).stream().map(
+                it -> new QueryTargetsResponse(it.getId(), it.getContent(), it.isAchieved())
+        ).toList();
+    }
+}

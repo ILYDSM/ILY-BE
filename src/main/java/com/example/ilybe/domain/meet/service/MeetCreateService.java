3f3406c84c83a35dd9eb828219dd6e4c3243ad59
seongwon <seongwon1290@dsm.hs.kr>
2023-11-23 11:34:00 +0900

    모임 생성시 목표 자동 생성

--- src/main/java/com/example/ilybe/domain/meet/service/MeetCreateService.java
+++ src/main/java/com/example/ilybe/domain/meet/service/MeetCreateService.java
@@ -3,6 +3,12 @@ package com.example.ilybe.domain.meet.service;
 import com.example.ilybe.domain.meet.domain.Meet;
 import com.example.ilybe.domain.meet.domain.repository.MeetRepository;
 import com.example.ilybe.domain.meet.presentation.dto.request.MeetCreateRequest;
+import com.example.ilybe.domain.target.domain.DetailTarget;
+import com.example.ilybe.domain.target.domain.SubTarget;
+import com.example.ilybe.domain.target.domain.Target;
+import com.example.ilybe.domain.target.domain.repository.DetailTargetRepository;
+import com.example.ilybe.domain.target.domain.repository.SubTargetRepository;
+import com.example.ilybe.domain.target.domain.repository.TargetRepository;
 import com.example.ilybe.domain.user.domain.User;
 import com.example.ilybe.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MeetCreateService {
     private final MeetRepository meetRepository;
     private final UserFacade userFacade;
+    private final TargetRepository targetRepository;
+    private final SubTargetRepository subTargetRepository;
+    private final DetailTargetRepository detailTargetRepository;
 
     @Transactional
     public Long execute(MeetCreateRequest request) {
@@ -27,6 +36,35 @@ public class MeetCreateService {
                 .personnel(request.getPersonnel())
                 .manager(user)
                 .build());
+
+        Target target = targetRepository.save(
+                Target.builder()
+                        .content("")
+                        .cycleDate(null)
+                        .cycleTerm(null)
+                        .cycleCount(null)
+                        .user(user)
+                        .build()
+        );
+
+        for (int i = 0; i <= 7; i++) {
+            SubTarget subTarget = subTargetRepository.save(
+                    SubTarget.builder()
+                            .target(target)
+                            .content("")
+                            .build());
+            int count = i * 8;
+            for (int j = count; j <= count + 7; j++) {
+                detailTargetRepository.save(
+                        DetailTarget.builder()
+                                .content("")
+                                .subTarget(subTarget)
+                                .build()
+                );
+            }
+        }
+
+        target.setMeet(meet);
 
         return meet.getId();
     }

3fe82ae666fa2545f4e9b079aac1671512d7d42a
csh0927 <sung060927@gmail.com>
2023-12-03 21:43:21 +0900

    모임 상세조회 API 수정

--- src/main/java/com/example/ilybe/domain/meet/presentation/dto/response/MeetDetailsResponse.java
+++ src/main/java/com/example/ilybe/domain/meet/presentation/dto/response/MeetDetailsResponse.java
@@ -1,10 +1,12 @@
 package com.example.ilybe.domain.meet.presentation.dto.response;
 
 import com.example.ilybe.domain.meet.domain.Type;
+import com.example.ilybe.domain.target.presentation.dto.response.SubTargetResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
@@ -12,10 +14,17 @@ import java.util.List;
 @Builder
 public class MeetDetailsResponse {
     private final Long meetId;
-    private final Long targetId;
     private final String title;
-    private final String content;
+    private final String meetContent;
     private final List<Type> type;
     private final Long personnel;
     private final Long participant;
+    private final Long targetId;
+    private final String targetContent;
+    private Integer cycleCount;
+    private Integer cycleTerm;
+    private LocalDate cycleDate;
+    private Boolean isAchieved;
+    private String theme;
+    private List<SubTargetResponse> subTargetResponseList;
 }
---
+++ src/main/java/com/example/ilybe/domain/meet/service/MeetBookmarkDetailsService.java
@@ -0,0 +1,9 @@
+package com.example.ilybe.domain.meet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MeetBookmarkDetailsService {
+}

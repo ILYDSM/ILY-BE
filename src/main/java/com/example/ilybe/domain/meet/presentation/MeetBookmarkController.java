449de7900676efc79ed98ba5a4b4e39dd52d68f5
csh0927 <sung060927@gmail.com>
2023-12-06 22:33:29 +0900

    즐겨찾기 한 모임 조회 API

--- src/main/java/com/example/ilybe/domain/meet/presentation/MeetBookmarkController.java
+++ src/main/java/com/example/ilybe/domain/meet/presentation/MeetBookmarkController.java
@@ -1,13 +1,18 @@
 package com.example.ilybe.domain.meet.presentation;
 
+import com.example.ilybe.domain.meet.presentation.dto.response.MeetListResponse;
 import com.example.ilybe.domain.meet.service.MeetBookmarkCreateService;
 import com.example.ilybe.domain.meet.service.MeetBookmarkDeleteService;
+import com.example.ilybe.domain.meet.service.MeetBookmarkDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("bookmark")
 @RequiredArgsConstructor
@@ -15,14 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MeetBookmarkController {
     private final MeetBookmarkCreateService meetBookmarkCreateService;
     private final MeetBookmarkDeleteService meetBookmarkDeleteService;
+    private final MeetBookmarkDetailsService meetBookmarkDetailsService;
 
     @PostMapping("/{id}")
-    public void MeetBookmarkCreate(@PathVariable("id") Long meetId) {
+    public void meetBookmarkCreate(@PathVariable("id") Long meetId) {
         meetBookmarkCreateService.execute(meetId);
     }
 
     @PatchMapping("/{id}")
-    public void MeetBookmarkDelete(@PathVariable("id") Long meetId) {
+    public void meetBookmarkDelete(@PathVariable("id") Long meetId) {
         meetBookmarkDeleteService.execute(meetId);
+    }
+
+    @GetMapping
+    public List<MeetListResponse> setMeetBookmarkDetailsService() {
+        return meetBookmarkDetailsService.execute();
     }
 }
--- src/main/java/com/example/ilybe/domain/meet/service/MeetBookmarkDetailsService.java
+++ src/main/java/com/example/ilybe/domain/meet/service/MeetBookmarkDetailsService.java
@@ -1,9 +1,31 @@
 package com.example.ilybe.domain.meet.service;
 
+import com.example.ilybe.domain.meet.presentation.dto.response.MeetListResponse;
+import com.example.ilybe.domain.user.domain.User;
+import com.example.ilybe.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class MeetBookmarkDetailsService {
+    private final UserFacade userFacade;
+
+    @Transactional
+    public List<MeetListResponse> execute() {
+        User user = userFacade.getCurrentUser();
+
+        return user.getMeets().stream()
+                .map(meet -> MeetListResponse.builder()
+                        .meetId(meet.getId())
+                        .title(meet.getTitle())
+                        .content(meet.getContent())
+                        .participant((long) meet.getUsers().size())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
